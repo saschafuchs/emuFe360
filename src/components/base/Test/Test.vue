d9f6f10e6c2836584b5b880347cf9c81b23ea827
saschafuchs <sascha.fuchs@webpros.com>
2023-01-24 13:12:31 +0100

    fix: typofix

--- src/components/base/Test/Test.vue
+++ src/components/base/Test/Test.vue
@@ -1,16 +1,11 @@
 <script lang="ts" setup>
 const props = defineProps({})
 
-const test = {
-  a: 1,
-  b: 2,
-  c: 3
-}
 </script>
 
 <template>
   <div class="test">
-    test
+    Hello World
   </div>
 </template>
 
